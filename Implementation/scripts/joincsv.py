470530e0c1e954b59d85b00782a90d4f395bbcb2
George Zhang <gzhang01@college.harvard.edu>
2016-04-17 00:49:00 -0400

    Complete data from 09-present

--- Implementation/scripts/joincsv.py
+++ Implementation/scripts/joincsv.py
@@ -1,5 +1,16 @@
-files = ["barbosa.csv", "clark.csv", "green.csv", "looney.csv", "speights.csv", "barnes.csv", "curry.csv", "iguodala.csv", "mcadoo.csv", "thompson.csv", "bogut.csv", "ezeli.csv", "livingston.csv", "rush.csv", "varejao.csv", "holiday.csv", "lee.csv", "kuzmic.csv", "armstrong.csv", "bazemore.csv"]
-names = ["Leandro Barbosa", "Ian Clark", "Draymond Green", "Kevon Looney", "Marreese Speights", "Harrison Barnes", "Stephen Curry", "Andre Iguodala", "James McAdoo", "Klay Thompson", "Andrew Bogut", "Festus Ezeli", "Shaun Livingston", "Brandon Rush", "Anderson Varejao", "Justin Holiday", "David Lee", "Ognjen Kuzmic", "Hilton Armstrong", "Kent Bazemore"]
+files = ["barbosa.csv", "clark.csv", "green.csv", "speights.csv", "barnes.csv", "curry.csv", "iguodala.csv", "mcadoo.csv", 
+"thompson.csv", "bogut.csv", "ezeli.csv", "livingston.csv", "rush.csv", "holiday.csv", "lee.csv", "kuzmic.csv", "bazemore.csv", 
+"blake.csv", "crawford.csv", "oneal.csv", "biedrins.csv", "jack.csv", "jefferson.csv", "jenkins.csv", "landry.csv", "tyler.csv", 
+"ellis.csv", "mcguire.csv", "moore.csv", "robinson.csv", "udoh.csv", "wright.csv", "amundson.csv", "carney.csv", "gadzuric.csv", 
+"law.csv", "lin.csv", "radmanovic.csv", "williams.csv", "george.csv", "hunter.csv", "maggette.csv", "morrow.csv", "tolliver.csv",
+"turiaf.csv", "watson.csv"]
+names = ["Leandro Barbosa", "Ian Clark", "Draymond Green", "Marreese Speights", "Harrison Barnes", "Stephen Curry", "Andre Iguodala", 
+"James McAdoo", "Klay Thompson", "Andrew Bogut", "Festus Ezeli", "Shaun Livingston", "Brandon Rush", "Justin Holiday", "David Lee", 
+"Ognjen Kuzmic", "Kent Bazemore", "Steve Blake", "Jordan Crawford", "Jermaine O'Neal", "Andris Biedrins", "Jarrett Jack", 
+"Richard Jefferson", "Charles Jenkins", "Carl Landry", "Jeremy Tyler", "Monta Ellis", "Dominic McGuire", "Mikki Moore", 
+"Nate Robinson", "Ekpe Udoh", "Dorell Wright", "Lou Amundson", "Rodney Carney", "Dan Gadzuric", "Acie Law", "Jeremy Lin", 
+"Vladimir Radmanovic", "Reggie Williams", "Devean George", "Chris Hunter", "Corey Maggette", "Anthony Morrow", "Anthony Tolliver", 
+"Ronny Turiaf", "C.J. Watson"]
 
 with open("../data/vis3/all.csv", "w") as f:
 	f.write("Name,Season,Age,Tm,Lg,Pos,G,GS,MP,FG,FGA,FG%,3P,3PA,3P%,2P,2PA,2P%,FT,FTA,FT%,ORB,DRB,TRB,AST,STL,BLK,TOV,PF,PTS\n")
