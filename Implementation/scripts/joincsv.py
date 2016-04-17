0a1bef0fc097db646e2b358ae487a7748cfba6a0
George Zhang <gzhang01@college.harvard.edu>
2016-04-16 22:10:07 -0400

    Took out middle names

--- Implementation/scripts/joincsv.py
+++ Implementation/scripts/joincsv.py
@@ -1,5 +1,5 @@
 files = ["barbosa.csv", "clark.csv", "green.csv", "looney.csv", "speights.csv", "barnes.csv", "curry.csv", "iguodala.csv", "mcadoo.csv", "thompson.csv", "bogut.csv", "ezeli.csv", "livingston.csv", "rush.csv", "varejao.csv", "holiday.csv", "lee.csv", "kuzmic.csv", "armstrong.csv", "bazemore.csv"]
-names = ["Leandro Barbosa", "Ian Clark", "Draymond Green", "Kevon Looney", "Marreese Speights", "Harrison Barnes", "Stephen Curry", "Andre Iguodala", "James Michael McAdoo", "Klay Thompson", "Andrew Bogut", "Festus Ezeli", "Shaun Livingston", "Brandon Rush", "Anderson Varejao", "Justin Holiday", "David Lee", "Ognjen Kuzmic", "Hilton Armstrong", "Kent Bazemore"]
+names = ["Leandro Barbosa", "Ian Clark", "Draymond Green", "Kevon Looney", "Marreese Speights", "Harrison Barnes", "Stephen Curry", "Andre Iguodala", "James McAdoo", "Klay Thompson", "Andrew Bogut", "Festus Ezeli", "Shaun Livingston", "Brandon Rush", "Anderson Varejao", "Justin Holiday", "David Lee", "Ognjen Kuzmic", "Hilton Armstrong", "Kent Bazemore"]
 
 with open("../data/vis3/all.csv", "w") as f:
 	f.write("Name,Season,Age,Tm,Lg,Pos,G,GS,MP,FG,FGA,FG%,3P,3PA,3P%,2P,2PA,2P%,FT,FTA,FT%,ORB,DRB,TRB,AST,STL,BLK,TOV,PF,PTS\n")
