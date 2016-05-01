9959a57d30e50576dc861df2614e13ce13a75c04
George Zhang <gzhang01@college.harvard.edu>
2016-05-01 14:29:25 -0400

    Updated styling

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
-$(function() {
-  $('a[href*=#]').on('click', function(e) {
-    e.preventDefault();
-    $('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top}, 500, 'linear');
+$(document).ready(function(){
+  $(function() {
+    $('a[href*=#]').on('click', function(e) {
+      e.preventDefault();
+      $('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top}, 500, 'linear');
+    });
   });
 });
